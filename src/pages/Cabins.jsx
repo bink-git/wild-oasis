280b6253794fdb04972d45d48f4e3399e826d184
bink-git <bink@email.ua>
2025-04-04 16:06:28 +0200

    add supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,19 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
+  useEffect(() => {
+    getCabins()
+      .then((data) => {
+        console.log('Cabins data:', data);
+      })
+      .catch((error) => {
+        console.error('Error fetching cabins:', error);
+      });
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
