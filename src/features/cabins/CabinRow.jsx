2fa14290d78ebcf80123ffbc316e4cfbeca59b04
bink-git <bink@email.ua>
2025-04-08 12:31:46 +0200

    fetch cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { formatCurrency } from '../../utils/helpers';
 
 const TableRow = styled.div`
   display: grid;
@@ -25,16 +26,30 @@ const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
-  font-family: "Sono";
+  font-family: 'Sono';
 `;
 
 const Price = styled.div`
-  font-family: "Sono";
+  font-family: 'Sono';
   font-weight: 600;
 `;
 
 const Discount = styled.div`
-  font-family: "Sono";
+  font-family: 'Sono';
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+const CabinRow = ({ cabin }) => {
+  return (
+    <TableRow role="row">
+      <Img src={cabin.image} alt={cabin.name} />
+      <Cabin>{cabin.name}</Cabin>
+      <div>{cabin.maxCapacity}</div>
+      <Price>{formatCurrency(cabin.regularPrice)} €</Price>
+      <Discount>{formatCurrency(cabin.discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+};
+export default CabinRow;
